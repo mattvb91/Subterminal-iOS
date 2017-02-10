a890eb87abb44ba587c3e7be924f23d83c8e562e
Matthias von Bargen <matt@mavon.ie>
2017-02-10 17:13:17 +0000

    start on baseform to link cancel event

---
+++ Subterminal/BaseForm.swift
@@ -0,0 +1,39 @@
+//
+//  BaseForm.swift
+//  Subterminal
+//
+//  Created by Matthias von Bargen on 10/02/2017.
+//  Copyright © 2017 Matthias von Bargen. All rights reserved.
+//
+
+import UIKit
+
+class BaseForm: UIViewController, UITextFieldDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    @IBAction func cancelAction(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+    }
+
+}
---
+++ Subterminal/Rig.swift
@@ -0,0 +1,30 @@
+//
+//  Rig.swift
+//  Subterminal
+//
+//  Created by Matthias von Bargen on 10/02/2017.
+//  Copyright © 2017 Matthias von Bargen. All rights reserved.
+//
+
+import Foundation
+
+class Rig {
+    
+    var container_manufacturer,
+    container_model,
+    container_serial,
+    container_date_in_use,
+    main_manufacturer,
+    main_model,
+    main_serial,
+    main_date_in_use,
+    reserve_manufacturer,
+    reserve_model,
+    reserve_serial,
+    reserve_date_in_use,
+    aad_manufacturer,
+    aad_model,
+    aad_serial,
+    aad_date_in_use: String?
+    
+}
