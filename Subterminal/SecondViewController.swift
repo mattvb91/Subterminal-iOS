dd633fcc3189e74cc3bc879f39ce59891dbc543f
Matthias von Bargen <matt@mavon.ie>
2017-02-10 12:21:03 +0000

    Initial Commit

---
+++ Subterminal/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Subterminal
+//
+//  Created by Matthias von Bargen on 10/02/2017.
+//  Copyright © 2017 Matthias von Bargen. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
