1979a6ef8f0f22b05499544c963347d5e5b281c6
Matthias von Bargen <matt@mavon.ie>
2017-02-21 19:23:55 +0000

    skydive rig form update

--- Subterminal/GearForm.swift
+++ Subterminal/GearForm.swift
@@ -23,6 +23,21 @@ class GearForm: Form {
 			getFormView().containerModel.text = getItem().container_model
 			getFormView().containerSerial.text = getItem().container_serial
 			getFormView().containerDateInUse.text = getItem().container_date_in_use
+			
+			getFormView().mainManufacturer.text = getItem().main_manufacturer
+			getFormView().mainModel.text = getItem().main_model
+			getFormView().mainSerial.text = getItem().main_serial
+			getFormView().mainDateInUse.text = getItem().main_date_in_use
+
+			getFormView().reserveManufacturer.text = getItem().reserve_manufacturer
+			getFormView().reserveModel.text = getItem().reserve_model
+			getFormView().reserveSerial.text = getItem().reserve_serial
+			getFormView().reserveDateInUse.text = getItem().reserve_date_in_use
+			
+			getFormView().aadManufacturer.text = getItem().aad_manufacturer
+			getFormView().aadModel.text = getItem().aad_model
+			getFormView().aadSerial.text = getItem().aad_serial
+			getFormView().aadDateInUse.text = getItem().aad_date_in_use
 		}
 		
 		self.view.addSubview(getFormView())
@@ -40,6 +55,18 @@ class GearForm: Form {
 		getItem().container_manufacturer = self.getFormView().containerManufacturer.text
 		getItem().container_model = self.getFormView().containerModel.text
 		getItem().container_serial = self.getFormView().containerSerial.text
+		
+		getItem().main_manufacturer = self.getFormView().mainManufacturer.text
+		getItem().main_model = self.getFormView().mainModel.text
+		getItem().main_serial = self.getFormView().mainSerial.text
+		
+		getItem().reserve_manufacturer = self.getFormView().reserveManufacturer.text
+		getItem().reserve_model = self.getFormView().reserveModel.text
+		getItem().reserve_serial = self.getFormView().reserveSerial.text
+		
+		getItem().aad_manufacturer = self.getFormView().aadManufacturer.text
+		getItem().aad_model = self.getFormView().aadModel.text
+		getItem().aad_serial = self.getFormView().aadSerial.text
 	}
 	
 	override func getFormView() -> GearFormView {
--- Subterminal/GearFormView.swift
+++ Subterminal/GearFormView.swift
@@ -20,10 +20,46 @@ class GearFormView: UIView, UITextFieldDelegate {
 	var containerSerial = UITextField()
 	var containerDateInUse = UITextField()
 	
-	var labelContainerManufacturer = UILabel()
-	var labelContainerModel = UILabel()
-	var labelContainerSerial = UILabel()
-	var labelContainerDateInUse = UILabel()
+	var containerTitle = UILabel()
+	var labelContainerManufacturer = Label(text: "Manufacturer")
+	var labelContainerModel = Label(text: "Model")
+	var labelContainerSerial = Label(text: "Serial")
+	var labelContainerDateInUse = Label(text: "Date in Use")
+	
+	var mainTitle = UILabel()
+	var labelMainManufacturer = Label(text: "Manufacturer")
+	var labelMainModel = Label(text: "Model")
+	var labelMainSerial = Label(text: "Serial")
+	var labelMainDateInUse = Label(text: "Date in Use")
+	
+	var mainManufacturer = TextFieldValidator()
+	var mainModel = UITextField()
+	var mainSerial = UITextField()
+	var mainDateInUse = UITextField()
+	
+	var reserveTitle = UILabel()
+	var labelReserveManufacturer = Label(text: "Manufacturer")
+	var labelReserveModel = Label(text: "Model")
+	var labelReserveSerial = Label(text: "Serial")
+	var labelReserveDateInUse = Label(text: "Date in Use")
+	
+	var reserveManufacturer = TextFieldValidator()
+	var reserveModel = UITextField()
+	var reserveSerial = UITextField()
+	var reserveDateInUse = UITextField()
+	
+	var aadTitle = UILabel()
+	var labelAadManufacturer = Label(text: "Manufacturer")
+	var labelAadModel = Label(text: "Model")
+	var labelAadSerial = Label(text: "Serial")
+	var labelAadDateInUse = Label(text: "Date in Use")
+	
+	var aadManufacturer = TextFieldValidator()
+	var aadModel = UITextField()
+	var aadSerial = UITextField()
+	var aadDateInUse = UITextField()
+	
+	var scrollView = UIScrollView()
 	
 	override init(frame: CGRect) {
 		super.init(frame: frame)
@@ -38,15 +74,12 @@ class GearFormView: UIView, UITextFieldDelegate {
 			}
 		}
 		
-		labelContainerManufacturer.text = "Manufacturer"
-		labelContainerModel.text = "Model"
-		labelContainerSerial.text = "Serial"
-		labelContainerDateInUse.text = "Date in use"
-	
 		containerManufacturer.delegate = self
 		containerManufacturer.add(validator: LenghtValidator(validationEvent: .perCharacter, min: 1))
 		containerManufacturer.validationBlock = requiredBlock
 
+		containerTitle.text = "Container"
+		containerTitle.font = UIFont.boldSystemFont(ofSize: 16)
 		containerManufacturer.setBottomBorder()
 		containerModel.setBottomBorder()
 		containerSerial.setBottomBorder()
@@ -56,16 +89,85 @@ class GearFormView: UIView, UITextFieldDelegate {
 		containerModel.clearButtonMode = UITextFieldViewMode.whileEditing
 		containerSerial.clearButtonMode = UITextFieldViewMode.whileEditing
 		
-		self.addSubview(containerManufacturer)
-		self.addSubview(containerModel)
-		self.addSubview(containerSerial)
-		self.addSubview(containerDateInUse)
+		mainTitle.text = "Main"
+		mainTitle.font = UIFont.boldSystemFont(ofSize: 16)
+		mainManufacturer.setBottomBorder()
+		mainModel.setBottomBorder()
+		mainSerial.setBottomBorder()
+		mainDateInUse.setBottomBorder()
 		
-		self.addSubview(labelContainerManufacturer)
-		self.addSubview(labelContainerModel)
-		self.addSubview(labelContainerSerial)
-		self.addSubview(labelContainerDateInUse)
+		mainManufacturer.clearButtonMode = UITextFieldViewMode.whileEditing
+		mainModel.clearButtonMode = UITextFieldViewMode.whileEditing
+		mainSerial.clearButtonMode = UITextFieldViewMode.whileEditing
 
+		reserveTitle.text = "Reserve"
+		reserveTitle.font = UIFont.boldSystemFont(ofSize: 16)
+		reserveManufacturer.setBottomBorder()
+		reserveModel.setBottomBorder()
+		reserveSerial.setBottomBorder()
+		reserveDateInUse.setBottomBorder()
+		
+		reserveManufacturer.clearButtonMode = UITextFieldViewMode.whileEditing
+		reserveModel.clearButtonMode = UITextFieldViewMode.whileEditing
+		reserveSerial.clearButtonMode = UITextFieldViewMode.whileEditing
+
+		aadTitle.text = "AAD"
+		aadTitle.font = UIFont.boldSystemFont(ofSize: 16)
+		aadManufacturer.setBottomBorder()
+		aadModel.setBottomBorder()
+		aadSerial.setBottomBorder()
+		aadDateInUse.setBottomBorder()
+		
+		aadManufacturer.clearButtonMode = UITextFieldViewMode.whileEditing
+		aadModel.clearButtonMode = UITextFieldViewMode.whileEditing
+		aadSerial.clearButtonMode = UITextFieldViewMode.whileEditing
+
+		self.addSubview(scrollView)
+		
+		scrollView.addSubview(containerTitle)
+		scrollView.addSubview(containerManufacturer)
+		scrollView.addSubview(containerModel)
+		scrollView.addSubview(containerSerial)
+		scrollView.addSubview(containerDateInUse)
+		
+		scrollView.addSubview(labelContainerManufacturer)
+		scrollView.addSubview(labelContainerModel)
+		scrollView.addSubview(labelContainerSerial)
+		scrollView.addSubview(labelContainerDateInUse)
+		
+		scrollView.addSubview(mainTitle)
+		scrollView.addSubview(mainManufacturer)
+		scrollView.addSubview(mainModel)
+		scrollView.addSubview(mainSerial)
+		scrollView.addSubview(mainDateInUse)
+		
+		scrollView.addSubview(labelMainManufacturer)
+		scrollView.addSubview(labelMainModel)
+		scrollView.addSubview(labelMainSerial)
+		scrollView.addSubview(labelMainDateInUse)
+
+		scrollView.addSubview(reserveTitle)
+		scrollView.addSubview(reserveManufacturer)
+		scrollView.addSubview(reserveModel)
+		scrollView.addSubview(reserveSerial)
+		scrollView.addSubview(reserveDateInUse)
+		
+		scrollView.addSubview(labelReserveManufacturer)
+		scrollView.addSubview(labelReserveModel)
+		scrollView.addSubview(labelReserveSerial)
+		scrollView.addSubview(labelReserveDateInUse)
+		
+		scrollView.addSubview(aadTitle)
+		scrollView.addSubview(aadManufacturer)
+		scrollView.addSubview(aadModel)
+		scrollView.addSubview(aadSerial)
+		scrollView.addSubview(aadDateInUse)
+		
+		scrollView.addSubview(labelAadManufacturer)
+		scrollView.addSubview(labelAadModel)
+		scrollView.addSubview(labelAadSerial)
+		scrollView.addSubview(labelAadDateInUse)
+		
 		setNeedsUpdateConstraints()
 	}
 	
@@ -77,11 +179,19 @@ class GearFormView: UIView, UITextFieldDelegate {
 	override func updateConstraints() {
 		if(!didSetupConstraints) {
 			self.autoPinEdgesToSuperviewEdges(with: UIEdgeInsets.zero)
+			scrollView.autoPinEdgesToSuperviewEdges(with: UIEdgeInsets.zero)
+			
+			let size = CGSize(width: UIScreen.main.bounds.width, height: 700)
+			scrollView.contentSize = size
+			scrollView.autoSetDimensions(to: size)
 		
 			let textFieldSize = CGSize(width: 180, height: 31)
 			
-			labelContainerManufacturer.autoPinEdge(toSuperviewEdge: .top, withInset: 80)
-			labelContainerManufacturer.autoPinEdge(toSuperviewEdge: .left, withInset: 10)
+			//Container
+			containerTitle.autoPinEdge(toSuperviewEdge: .left, withInset: 10)
+			
+			labelContainerManufacturer.autoPinEdge(.top, to: .bottom, of: containerTitle, withOffset: 8)
+			labelContainerManufacturer.autoPinEdge(.left, to: .left, of: containerTitle)
 			
 			labelContainerModel.autoPinEdge(.left, to: .right, of: labelContainerManufacturer, withOffset: 100)
 			labelContainerModel.autoPinEdge(.top, to: .top, of: labelContainerManufacturer)
@@ -107,7 +217,102 @@ class GearFormView: UIView, UITextFieldDelegate {
 			containerDateInUse.autoPinEdge(.top, to: .bottom, of: labelContainerDateInUse, withOffset: 8)
 			containerDateInUse.autoPinEdge(.left, to: .left, of: labelContainerDateInUse)
 			containerDateInUse.autoSetDimensions(to: textFieldSize)
+			
+			//Container
+			mainTitle.autoPinEdge(.top, to: .bottom, of: containerSerial, withOffset:  12)
+			mainTitle.autoPinEdge(.left, to: .left, of: containerSerial)
+			
+			labelMainManufacturer.autoPinEdge(.top, to: .bottom, of: mainTitle, withOffset: 8)
+			labelMainManufacturer.autoPinEdge(.left, to: .left, of: mainTitle)
+			
+			labelMainModel.autoPinEdge(.left, to: .right, of: labelMainManufacturer, withOffset: 100)
+			labelMainModel.autoPinEdge(.top, to: .top, of: labelMainManufacturer)
+			
+			mainManufacturer.autoPinEdge(.top, to: .bottom, of: labelMainManufacturer, withOffset: 8)
+			mainManufacturer.autoPinEdge(.left, to: .left, of: labelMainManufacturer)
+			mainManufacturer.autoSetDimensions(to: textFieldSize)
+			
+			mainModel.autoPinEdge(.top, to: .bottom, of: labelMainModel, withOffset: 8)
+			mainModel.autoPinEdge(.left, to: .left, of: labelMainModel)
+			mainModel.autoSetDimensions(to: textFieldSize)
+			
+			labelMainSerial.autoPinEdge(.top, to: .bottom, of: mainManufacturer, withOffset: 10)
+			labelMainSerial.autoPinEdge(.left, to: .left, of: mainManufacturer)
+			
+			labelMainDateInUse.autoPinEdge(.left, to: .left, of: labelMainModel)
+			labelMainDateInUse.autoPinEdge(.top, to: .top, of: labelMainSerial)
+			
+			mainSerial.autoPinEdge(.top, to: .bottom, of: labelMainSerial, withOffset: 8)
+			mainSerial.autoPinEdge(.left, to: .left, of: labelMainSerial)
+			mainSerial.autoSetDimensions(to: textFieldSize)
+			
+			mainDateInUse.autoPinEdge(.top, to: .bottom, of: labelMainDateInUse, withOffset: 8)
+			mainDateInUse.autoPinEdge(.left, to: .left, of: labelMainDateInUse)
+			mainDateInUse.autoSetDimensions(to: textFieldSize)
+			
+			//Reserve
+			reserveTitle.autoPinEdge(.top, to: .bottom, of: mainSerial, withOffset:  12)
+			reserveTitle.autoPinEdge(.left, to: .left, of: mainSerial)
+			
+			labelReserveManufacturer.autoPinEdge(.top, to: .bottom, of: reserveTitle, withOffset: 8)
+			labelReserveManufacturer.autoPinEdge(.left, to: .left, of: reserveTitle)
+			
+			labelReserveModel.autoPinEdge(.left, to: .right, of: labelReserveManufacturer, withOffset: 100)
+			labelReserveModel.autoPinEdge(.top, to: .top, of: labelReserveManufacturer)
+			
+			reserveManufacturer.autoPinEdge(.top, to: .bottom, of: labelReserveManufacturer, withOffset: 8)
+			reserveManufacturer.autoPinEdge(.left, to: .left, of: labelReserveManufacturer)
+			reserveManufacturer.autoSetDimensions(to: textFieldSize)
+			
+			reserveModel.autoPinEdge(.top, to: .bottom, of: labelReserveModel, withOffset: 8)
+			reserveModel.autoPinEdge(.left, to: .left, of: labelReserveModel)
+			reserveModel.autoSetDimensions(to: textFieldSize)
+			
+			labelReserveSerial.autoPinEdge(.top, to: .bottom, of: reserveManufacturer, withOffset: 10)
+			labelReserveSerial.autoPinEdge(.left, to: .left, of: reserveManufacturer)
+			
+			labelReserveDateInUse.autoPinEdge(.left, to: .left, of: reserveModel)
+			labelReserveDateInUse.autoPinEdge(.top, to: .top, of: labelReserveSerial)
+			
+			reserveSerial.autoPinEdge(.top, to: .bottom, of: labelReserveSerial, withOffset: 8)
+			reserveSerial.autoPinEdge(.left, to: .left, of: labelReserveSerial)
+			reserveSerial.autoSetDimensions(to: textFieldSize)
+			
+			reserveDateInUse.autoPinEdge(.top, to: .bottom, of: labelReserveDateInUse, withOffset: 8)
+			reserveDateInUse.autoPinEdge(.left, to: .left, of: labelReserveDateInUse)
+			reserveDateInUse.autoSetDimensions(to: textFieldSize)
 
+			//AAD
+			aadTitle.autoPinEdge(.top, to: .bottom, of: reserveSerial, withOffset:  12)
+			aadTitle.autoPinEdge(.left, to: .left, of: reserveSerial)
+			
+			labelAadManufacturer.autoPinEdge(.top, to: .bottom, of: aadTitle, withOffset: 8)
+			labelAadManufacturer.autoPinEdge(.left, to: .left, of: aadTitle)
+			
+			labelAadModel.autoPinEdge(.left, to: .right, of: labelAadManufacturer, withOffset: 100)
+			labelAadModel.autoPinEdge(.top, to: .top, of: labelAadManufacturer)
+			
+			aadManufacturer.autoPinEdge(.top, to: .bottom, of: labelAadManufacturer, withOffset: 8)
+			aadManufacturer.autoPinEdge(.left, to: .left, of: labelAadManufacturer)
+			aadManufacturer.autoSetDimensions(to: textFieldSize)
+			
+			aadModel.autoPinEdge(.top, to: .bottom, of: labelAadModel, withOffset: 8)
+			aadModel.autoPinEdge(.left, to: .left, of: labelAadModel)
+			aadModel.autoSetDimensions(to: textFieldSize)
+			
+			labelAadSerial.autoPinEdge(.top, to: .bottom, of: aadManufacturer, withOffset: 10)
+			labelAadSerial.autoPinEdge(.left, to: .left, of: aadManufacturer)
+			
+			labelAadDateInUse.autoPinEdge(.left, to: .left, of: aadModel)
+			labelAadDateInUse.autoPinEdge(.top, to: .top, of: labelAadSerial)
+			
+			aadSerial.autoPinEdge(.top, to: .bottom, of: labelAadSerial, withOffset: 8)
+			aadSerial.autoPinEdge(.left, to: .left, of: labelAadSerial)
+			aadSerial.autoSetDimensions(to: textFieldSize)
+			
+			aadDateInUse.autoPinEdge(.top, to: .bottom, of: labelAadDateInUse, withOffset: 8)
+			aadDateInUse.autoPinEdge(.left, to: .left, of: labelAadDateInUse)
+			aadDateInUse.autoSetDimensions(to: textFieldSize)
 			
 			self.didSetupConstraints = true
 		}
